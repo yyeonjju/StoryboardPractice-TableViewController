dbeb4dfcfdd28f0bdae72c6f03ce3e3845fcaeb2
yyeonjju <llilyha123@gmail.com>
2024-05-23 16:30:24 +0900

    Initial Commit

---
+++ SesacAssignment0523/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SesacAssignment0523
+//
+//  Created by 하연주 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
